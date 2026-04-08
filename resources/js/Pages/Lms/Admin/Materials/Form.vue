29d5ed49c8cb9b1cf943f7fc96a9235f72507fd2
Andrey Kleinos <goodmena@yandex.ru>
2026-04-08 20:09:59 +0300

    change order in forms

--- resources/js/Pages/Lms/Admin/Materials/Form.vue
+++ resources/js/Pages/Lms/Admin/Materials/Form.vue
@@ -44,7 +44,7 @@
               :key="idx"
               class="flex items-start gap-3 rounded-xl border border-gray-200 bg-white p-3"
             >
-              <div class="flex-1 space-y-2">
+              <div class="min-w-0 flex-1 space-y-2">
                 <RInput
                   v-model="file.title"
                   placeholder="Название файла"
@@ -63,9 +63,17 @@
                   <span class="text-sm text-gray-500 group-hover:text-gray-700">Выберите файл</span>
                 </button>
               </div>
-              <button type="button" class="mt-1 cursor-pointer text-gray-400 hover:text-red-500" @click="form.files.splice(idx, 1)">
-                <svg class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1.5"><path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" /></svg>
-              </button>
+              <div class="flex shrink-0 items-start gap-1 pt-0.5">
+                <RButton v-if="idx > 0" type="button" variant="ghost" size="sm" iconOnly title="Выше" @click="moveMaterialFile(idx, -1)">
+                  <template #icon><ChevronUpIcon class="h-4 w-4" /></template>
+                </RButton>
+                <RButton v-if="idx < form.files.length - 1" type="button" variant="ghost" size="sm" iconOnly title="Ниже" @click="moveMaterialFile(idx, 1)">
+                  <template #icon><ChevronDownIcon class="h-4 w-4" /></template>
+                </RButton>
+                <button type="button" class="mt-0.5 cursor-pointer text-gray-400 hover:text-red-500" title="Удалить" @click="form.files.splice(idx, 1)">
+                  <svg class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1.5"><path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" /></svg>
+                </button>
+              </div>
             </div>
           </div>
           <button
@@ -105,6 +113,7 @@
 <script setup>
 import { ref } from 'vue'
 import { Link, useForm } from '@inertiajs/vue3'
+import { ChevronDownIcon, ChevronUpIcon } from '@heroicons/vue/24/outline'
 import LmsAdminLayout from '@/Layouts/LmsAdminLayout.vue'
 import RichTextEditor from '@/Components/RichTextEditor.vue'
 import MediaPickerModal from '@/Components/MediaPickerModal.vue'
@@ -139,6 +148,14 @@ const form = useForm({
 
 function addFile() {
   form.files.push({ id: null, title: '', file_path: '', file_name: '', file_size: null, _uploading: false })
+}
+
+function moveMaterialFile(idx, delta) {
+  const newIdx = idx + delta
+  if (newIdx < 0 || newIdx >= form.files.length) return
+  const arr = [...form.files]
+  ;[arr[idx], arr[newIdx]] = [arr[newIdx], arr[idx]]
+  form.files = arr
 }
 
 function openMatFilePicker(idx) {
